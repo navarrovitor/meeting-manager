483e8345ad30f49debaad7df49edf263931c8cf3
vitor navarro <vitor.san.nav.nexus@gmail.com>
2021-09-03 18:05:09 -0300

    create room repository

---
+++ src/main/java/com/vitor/meeting/repository/RoomRepository.java
@@ -0,0 +1,11 @@
+package com.vitor.meeting.repository;
+
+import com.vitor.meeting.model.Room;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room, Long> {
+
+}
